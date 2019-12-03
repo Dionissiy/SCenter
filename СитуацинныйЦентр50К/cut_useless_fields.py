a102a346e66cc34f9b9792df16ae92f0c5d51b33
Dionissiy <32749678+Dionissiy@users.noreply.github.com>
2019-12-03 19:46:23 +0300

    Delete cut_useless_fields.py

--- СитуацинныйЦентр50К/cut_useless_fields.py
+++
@@ -1,9 +0,0 @@
-def cut_useless_fields(vacancies):
-    for x in vacancies:
-        x.pop('\ufeffconditions', None)
-        x.pop('Год', None)
-        x.pop('Месяц', None)
-        x.pop('industry', None)
-        x.pop('education', None)
-        x.pop('requirements', None)
-        x.pop('responsibilities', None)
